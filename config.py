4dd2396bd69966139d2737eaa7ca8aaf10a0903e
y3mvl <35850897+y3mvl@users.noreply.github.com>
2024-12-31 18:57:43 +0000

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,3 @@ if (os.environ.get("BINANCE_ENABLED") == 'Y'):
 
   BINANCE_API_KEY=os.environ.get("API_KEY")
   BINANCE_API_SECRET=os.environ.get("API_SECRET")
-
-print(os.environ);
-print(BINANCE_ENABLED);
-print(os.environ.get("BINANCE_ENABLED"));
